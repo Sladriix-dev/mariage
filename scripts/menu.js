d48f0fb05541f96e7e0d4735fe8b83d5be3f1cf6
Sladriix <sladriix@gmail.com>
2024-02-07 12:46:15 +0100

    add features pour ne plus avoir le scroll lorsque le hamburger menu est ouvert

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const body = document.body;
     const menuToggle = document.querySelector('.menu-toggle');
     const menu = document.querySelector('nav ul');
 
     menuToggle.addEventListener('click', function() {
+        const menuIsActive = menu.classList.contains('active');
+
         this.classList.toggle('active');
         menu.classList.toggle('active');
+
+        if (menuIsActive) {
+            body.style.overflow = ''; // Réactive le défilement
+        } else {
+            body.style.overflow = 'hidden'; // Désactive le défilement
+        }
     });
 });
