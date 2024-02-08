291fcb47f81018a15c9d4b113a02e3d85bb49d86
Sladriix <sladriix@gmail.com>
2024-02-08 10:58:25 +0100

    [add] tous les derniers changements

--- scripts/sendForm.js
+++ scripts/sendForm.js
@@ -1,56 +1,94 @@
-document
-  .getElementById("rsvp-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Empêche l'envoi standard du formulaire
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("rsvp-form");
+  const confirmationMessage = document.getElementById("confirmation-message");
 
-    // Déterminez si l'utilisateur a répondu "Oui" ou "Non"
-    var participation = document.querySelector(
-      'input[name="participation"]:checked'
-    )?.value; // Utilisation de l'opérateur facultatif pour éviter les erreurs si rien n'est sélectionné
-    var invites = document.getElementById("invites").value;
-    var commentaires = document.getElementById("commentaires").value;
+  // Restaurer les données du formulaire si elles existent dans localStorage
+  form.nom.value = localStorage.getItem("nom") || "";
+  form.prenom.value = localStorage.getItem("prenom") || "";
+  form.email.value = localStorage.getItem("email") || "";
+  form.invites.value = localStorage.getItem("invites") ?? "";
+  form.commentaires.value = localStorage.getItem("commentaires") || "";
 
-    // Assurez-vous que la participation a été sélectionnée
-    if (!participation) {
-      alert("Veuillez sélectionner une option de participation.");
-      return; // Arrêtez la fonction ici si aucune option de participation n'est sélectionnée
+  // Fonction pour vérifier si les champs requis sont remplis et si 'invites' est un nombre entier
+  function validateForm() {
+    if (
+      !form.nom.value ||
+      !form.prenom.value ||
+      !form.email.value ||
+      !form.querySelector('input[name="participation"]:checked')
+    ) {
+      alert("Veuillez remplir tous les champs requis.");
+      return false;
+    }
+
+    // Vérifie si 'invites' est un nombre entier
+    if (form.invites.value && !Number.isInteger(Number(form.invites.value))) {
+      alert("Veuillez entrer un nombre entier pour le nombre d'invités.");
+      return false;
+    }
+
+    return true;
+  }
+
+  // Fonction pour sauvegarder les données du formulaire
+  function saveFormData() {
+    localStorage.setItem("nom", form.nom.value);
+    localStorage.setItem("prenom", form.prenom.value);
+    localStorage.setItem("email", form.email.value);
+    localStorage.setItem("invites", form.invites.value);
+    localStorage.setItem("commentaires", form.commentaires.value);
+  }
+
+  // Écouteur d'événements sur les champs du formulaire pour sauvegarder les données
+  form.addEventListener("input", saveFormData);
+
+  // Écouteur d'événements sur le formulaire pour la soumission
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Vérifier la validation du formulaire
+    if (!validateForm()) {
+      return;
     }
 
     // Préparez les paramètres en fonction de la réponse
     var emailParams = {
-      from_name:
-        document.getElementById("nom").value +
-        " " +
-        document.getElementById("prenom").value,
-      reply_to: document.getElementById("email").value,
+      from_name: form.nom.value + " " + form.prenom.value,
+      reply_to: form.email.value,
       message:
-        participation === "oui" ? "sera présent." : "ne sera pas présent.",
-      nbr_invites:
-        invites > 0 ? "Nombre d'invités : " + invites : "Sans invité", // Préfixe ajouté pour clarifier dans l'email
-      comments: commentaires
-        ? "Il/Elle a laissé un commentaire : " + commentaires
+        form.querySelector('input[name="participation"]:checked').value ===
+        "oui"
+          ? "sera présent."
+          : "ne sera pas présent.",
+      nbr_invites: form.invites.value
+        ? "Nombre d'invités : " + form.invites.value
+        : "Sans invité",
+      comments: form.commentaires.value
+        ? "Commentaire : " + form.commentaires.value
         : "Aucun commentaire.",
     };
 
+    // Envoyez l'email avec EmailJS
     emailjs.send("service_h3haa2r", "template_12bzfm7", emailParams).then(
       function (response) {
         console.log("SUCCESS!", response.status, response.text);
-
-        // Afficher le message de confirmation personnalisé
-        var confirmationMessage = document.getElementById(
-          "confirmation-message"
-        );
-        confirmationMessage.classList.remove("hidden");
-        confirmationMessage.classList.add("show-confirmation");
-
-        // Optionnel : cacher le message après quelques secondes
-        setTimeout(function () {
-          confirmationMessage.classList.add("hidden");
-        }, 5000);
+        showConfirmationPopup();
+        form.reset();
+        localStorage.clear();
       },
       function (error) {
         console.log("FAILED...", error);
-        // Ici, vous pourriez gérer l'erreur et afficher un message approprié
       }
     );
   });
+
+  function showConfirmationPopup() {
+    const confirmationMessage = document.getElementById('confirmation-message');
+    confirmationMessage.classList.add('show'); 
+
+    // Pour cacher à nouveau l'élément après l'affichage
+    setTimeout(() => {
+      confirmationMessage.classList.remove("show");
+    }, 2500); // L'élément sera caché après 2.5 secondes
+  }
+});
